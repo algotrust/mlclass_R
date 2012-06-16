f833b08ed5fc73732aba4d73b3a7cd8e8c5d5fa0
Nikita <nikita@nikita-Aspire-5920G.(none)>
2012-06-16 16:48:36 +0400

    Added code to calculate initial cost and gradient in ex2.R

--- ex2/ex2.R
+++ ex2/ex2.R
@@ -1,3 +1,4 @@
+#!/usr/bin/Rscript
 library(ggplot2)
 #Closing all the plots and resetting all the variables in the workspace
 graphics.off()
@@ -8,14 +9,36 @@ source("sigmoid.R")
 source("costFunction.R")
 source("predict.R")
 
+#Reading excercise data
 data <- read.delim("ex2data1.txt", header=FALSE, sep=",")
 data$V3 <- factor(data$V3)
 
+#=======Part 1. Plotting exam scores and admissions==============
 p <- ggplot(data, aes(x = V1, y = V2, color = V3, shape = V3)) +
    geom_point() +
    scale_x_continuous("Exam 1 score") +
    scale_y_continuous("Exam 2 score") +
    scale_colour_hue(name = "Admitted?", labels=c("No", "Yes")) +
-   scale_shape(name = "Admitted?", labels=c("No", "Yes"))
-   
+   scale_shape(name = "Admitted?", labels=c("No", "Yes"))   
 print(p)
+#=========Part 2. Computing cost and gradient ==================
+#Initializing input variables X and output variables y
+X <- data[,c(1,2)]
+y <- data[,3]
+
+m <- nrow(X)  
+n <- ncol(X)
+
+#Adding intercept term and initializing parameters
+X$V0 <- rep(1,m)
+X <- X[c('V0','V1','V2')]
+initial_theta <- rep(0,n+1)
+
+cost <- cost(initial_theta,X,y)
+grad <- gradient(initial_theta,X,y)
+print(paste('Cost at initial theta (zeros):', cost))
+print("Gradient at initial theta (zeros):")
+print(grad)
+#Pausing execution
+cat("Hit <enter> to continue...")
+readline()
