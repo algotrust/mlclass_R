a81431ad42e8a25d8d3cce905e8a300e261812fb
Nikita Barsukov <nbarsukov@zendesk.com>
2012-08-05 23:53:46 +0400

    Rewrote to speed up the script

--- ex2/ex2_reg.R
+++ ex2/ex2_reg.R
@@ -1,4 +1,10 @@
 #!/usr/bin/Rscript 
+# Advanced branch:
+# Compute decision boundaries, accuracy on training set
+# with different parameters of Lambda.
+# Plot different decision boundaries, and training accuracies,
+# correspndng different values.
+
 library(ggplot2)
 library(reshape)
 #Closing all the plots and resetting all the variables in the workspace
@@ -47,39 +53,27 @@ opt_params <- unlist(o["par"])
 # Printing values
 cat(sprintf('Cost at theta found by optim: %f\n\n', o["value"]))
 #=====Plotting decision boundary ========
-# Code taken from here:
+# Inspiration taken from here:
 # http://ygc.name/2011/10/26/machine-learning-5-2-regularized-logistic-regression/
 
 #Initializing argument vectors with arbitrary value
 u <- seq(min(data[1]),max(data[1]), len=200)
 v <- seq(min(data[2]),max(data[2]), len=200)
-#Initializing empty matrix 
-z0 <- matrix(0, length(u), length(v))
-#Filling matrix values: i-jth value there equals
-# value of the regression function we found 
-# with values u(i) and v(j)
-for (i in 1:length(u)) {
-  for (j in 1:length(v)) {
-    features <- mapFeature(u[i],v[j])
-    z0[i,j] <- as.matrix(features) %*% opt_params
-  }
-}
-# Naming rows and colums of our matrix
-# And preparing it for plotting
-rownames(z0) <- as.character(u)
-colnames(z0) <- as.character(v)
-z0.melted <- melt(z0)
-z0.melted <- data.frame(z0.melted, lambda=lambda)
-zz <- rbind(z0.melted)
-#Lambda is a factor argument:
-# We don't want it to be on x or y axis
-zz$lambda <- factor(zz$lambda)
-colnames(zz) <- c("u", "v", "z", "lambda")
+#Initializing vector with cartesian product of vectors u and v
+product<-expand.grid(u,v)
+#Mapping initial two arguments to a 6th power polynome
+polynome_features <- mapFeature(product$Var1,product$Var2)
+# Initializing dataframe of values with regularized optimal parameters
+z <- as.data.frame(as.matrix(polynome_features) %*% opt_params)
+z$u <- product$Var1
+z$v <- product$Var2
+z$lambda <- lambda
+z$lambda <- factor(z$lambda)
 # using geom_countour method from ggplot2 package
 # note: we reinitialized colour and shape variables in aes
 # because we assigned different vectors to these variables 
 # when we drew p the first time
-p <- p+geom_contour(data=zz, aes(x=u, y=v, z=z, group=lambda, colour=lambda,shape=lambda),bins=1)
+p <- p+geom_contour(data=z, aes(x=u, y=v, z=V1, group=lambda, colour=lambda,shape=lambda),bins=1)
 print(p)
  #==== Prediction and accuracies =========
 # Calculating accuracy of our algorithm on training set
