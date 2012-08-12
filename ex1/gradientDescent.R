e71635812779e0afe268b2ddcea6eeb79d835932
Nikita Barsukov <nbarsukov@zendesk.com>
2012-08-12 17:26:25 +0400

    Fixed gradient descent function

--- ex1/gradientDescent.R
+++ ex1/gradientDescent.R
@@ -1,11 +1,10 @@
-source('computeCost.R')
 gradientDescent <- function(X, y, theta, alpha, num_iters){
   m <- length(y)
   X <- as.matrix(X)
-  J_hist <- rep(num_iters,0)
-  for (i in num_iters){
-    theta <- theta - alpha * t(X) %*% (X %*% initial_theta - y) / m
-    print( computeCost(X,y,theta) )
+  J_hist <- 0
+  for (i in 1:num_iters){
+    theta <- theta - alpha * t(X) %*% (X %*% theta - y) / m
+    J_hist[i] = computeCost(X,y,theta)
   }
   return(list('theta' = theta,'J_hist'=J_hist))
 }
