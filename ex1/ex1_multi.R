d876fce9ef10fa79ca39d8cd5d5a762c47da6c4a
Nikita Barsukov <nbarsukov@zendesk.com>
2012-08-26 10:39:05 +0300

    copied ex1_multi.R from solved to master

--- ex1/ex1_multi.R
+++ ex1/ex1_multi.R
@@ -24,8 +24,8 @@ n <- ncol(X)
 
 #======Part 1: normalizing features ===========
 X.norm <- featureNormalize(X)
-mju <- apply(X,2,median)
-stddev <- apply(X,2,sd)
+mjus <- sapply(X,mean)
+stddevs <- sapply(X,sd)
 X.norm$V0 <- rep(1,m)
 X.norm <- X.norm[c('V0','V1','V2')]
 
@@ -34,16 +34,23 @@ X.norm <- X.norm[c('V0','V1','V2')]
 initial_theta <- rep(0,n+1)
 iterations <- 1500
 alpha <- 0.01
-
-print("Running gradient descent...")
+cat(sprintf('Cost at initial parameters (all zeros): %f\n',computeCost(X.norm,y,initial_theta)))
+cat("Running gradient descent...\n")
 opt_params <- gradientDescent(X.norm,y,initial_theta,alpha,iterations)
 
 opt_theta <- unlist(opt_params['theta'])
 names(opt_theta)<-c('theta0','theta1','theta2')
 
-print('Optimal parameters found by gradient descent: ')
+cat('Optimal parameters found by gradient descent:\n')
 print(opt_theta)
-cat(sprintf('Cost at optimal parameters: %f',computeCost(X,y,opt_theta)))
+cat(sprintf('Cost at optimal parameters: %f\n\n',computeCost(X.norm,y,opt_theta)))
 
 #=========== Part 3: Normal equations ========
-
+cat("Calculating optimal parameters using normal equations.\n")
+X$V0 <- rep(1,m)
+X <- X[c('V0','V1','V2')]
+theta_norm_eqn = as.numeric(normalEqn(X,y))
+names(theta_norm_eqn) = c('theta0','theta1','theta2')
+cat('Optimal parameters found by normal equations:\n')
+print(theta_norm_eqn)
+cat(sprintf('Cost at optimal parameters: %f\n\n',computeCost(X,y,theta_norm_eqn)))
