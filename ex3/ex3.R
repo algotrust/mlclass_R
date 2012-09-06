c2bc4117ce1a077aa3c5c08986ebf0755dcc1c51
Nikita Barsukov <nbarsukov@zendesk.com>
2012-09-06 18:25:36 +0300

    New comments, sigmoid function

--- ex3/ex3.R
+++ ex3/ex3.R
@@ -9,18 +9,20 @@ source('oneVsAll.R')
 source('predict.R')
 source('predictOneVsAll.R')
 
-data <- readMat('ex3data1.mat')
-X <- unlist(data['X'])
+## =========== Part 1: Loading and Visualizing Data =============
+cat('Loading and visualizing data\n\n')  
+raw_data <- readMat('ex3data1.mat')
+X <- unlist(raw_data['X'])
 # There are 5000 training examples, each containing 400 pixels. 
 # THus we should convert X from a vector with 2.000.000 elements to 
-# 5000 X 400 matrix
+# 5000x400 matrix
 X <- matrix(X, nrow=5000)
-y <-unlist(data['y'])
-# Plotting 100 random image matrices
-# First: Specifying plot parameters: pin - size of single image (in), mfrow - dimensions of matrix of plots
-# mai - margins
-par(pin=c(0.2,0.2), mfrow=c(10,10), mai=c(0.01,0.01,0.01,0.01))
+y <-unlist(raw_data['y'])
 
+# Plotting 100 random image matrices
+# Specifying plot parameters: pin - size of single image (in), mfrow - dimensions of matrix of plots
+# mai - margins
+par(pin=c(0.2,0.2), mfrow=c(10,10), mai=c(0,0,0,0))
 # Tipped off here: http://stackoverflow.com/questions/5638462/r-image-of-a-pixel-matrix
 # And here: http://stackoverflow.com/questions/7991056/change-origin-in-image-plot-of-matrix-in-r
 for (i in 1:100) {
@@ -28,3 +30,5 @@ for (i in 1:100) {
         axes=FALSE,
         col = grey(seq(0, 1, length = 256)))  
 }
+## ============ Part 2: Vectorize Logistic Regression ============
+cat('Training One-vs-All Logistic Regression...\n\n')  
